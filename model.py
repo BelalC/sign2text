648ff07d2acf98a6efc85eb53e31ee19caf0a44d
bchaudhary <belalc80@gmail.com>
2017-04-21 18:39:46 +0200

    default to pre-trained weights for transfer learning

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from keras.applications import ResNet50, VGG16, InceptionV3, Xception
 from keras.layers import Flatten, Dense, Dropout, Input
 from keras.models import Sequential, Model
 
-# map model names to classes
+# Map model names to classes
 MODELS = {
     "vgg16": VGG16,
     "inception": InceptionV3,
@@ -13,6 +13,9 @@ MODELS = {
     "resnet": ResNet50
 }
 
+# Define path to pre-trained classification block weights - this is
+vgg_weights_path = "weights/vgg16_pretrain_weights.h5"
+# res_weights_path = "weights/vgg16_pretrain_weights.h5"
 
 def create_model(model, model_weights_path=None, top_model=True):
     """Create custom model for transfer learning
@@ -27,7 +30,7 @@ def create_model(model, model_weights_path=None, top_model=True):
     model: str
         choose which pre-trained Keras deep learning model to use for the 'bottom' layers of the custom model
     model_weights_path: str
-        path to pre-trained weights, if available
+        optional path to weights for classification block; otherwise, pre-trained weights will be loaded
     top_model: bool
         whether to include custom classification block, or to load model 'without top' to extract features
 
@@ -41,10 +44,12 @@ def create_model(model, model_weights_path=None, top_model=True):
     if model not in MODELS.keys():
         raise AssertionError("The model parameter must be a key in the `MODELS` dictionary")
 
-    # Create pre-trained model without classification block
+    # Create pre-trained model for feature extraction, without classification block
     print("[INFO] loading %s..." % (model,))
     model = MODELS[model](include_top=False,
                           input_tensor=Input(shape=(224, 224, 3)))
+
+    # For transfer learning
     if top_model:
         # Create classification block
         top_model = Sequential()
@@ -53,14 +58,22 @@ def create_model(model, model_weights_path=None, top_model=True):
         top_model.add(Dropout(0.5))
         top_model.add(Dense(26, activation='softmax'))
 
-        # Join model + classification block
+        # Join pre-loaded model + classification block
         print("[INFO] creating model.")
         my_model = Model(inputs=model.input,
                          outputs=top_model(model.output))
 
-    if model_weights_path is not None:
-        # Load pre-trained model weights
+        # Load weights for classification block
         print("[INFO] loading model weights.")
-        my_model.load_weights(model_weights_path)
+        if model_weights_path is not None:
+            # user-supplied weights
+            my_model.load_weights(model_weights_path)
+        elif model == "vgg16":
+            # pre-trained weights for transfer learning with VGG16
+            my_model.load_weights(vgg_weights_path)
+        elif model == "resnet":
+            # pre-trained weights for transfer learning with ResNet50
+            print("ResNet50 pre-trained weights are not available yet, please use VGG16 for now!")
+            # my_model.load_weights(res_weights_path)
 
     return my_model
